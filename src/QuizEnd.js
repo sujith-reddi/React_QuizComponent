fb1b91cff459c4f15d578705ae6c9e8b0d513590
*sujith* <31003697+sujith45@users.noreply.github.com>
2021-11-21 19:34:47 +0530

    Add files via upload

---
+++ src/QuizEnd.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class QuizEnd extends Component {
+    handleResetClick() {
+        return this.props.resetClickHandler()
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Thanks for playing!</p>
+                <a href='' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
+            </div>
+        );
+    }
+}
+
+export default QuizEnd;
